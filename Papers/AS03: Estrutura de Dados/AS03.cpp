ba74dc86e399d5850ede649472fae8599be5efbe
ThiagoHN <thiagohnogueira01@gmail.com>
2021-08-22 12:04:00 -0300

    [update] Activity: solution to AS03

--- Papers/AS03: Estrutura de Dados/AS03.cpp
+++ Papers/AS03: Estrutura de Dados/AS03.cpp
@@ -4,7 +4,7 @@
  * Nome: Thiago Henriques Nogueira
  * Matricula: 655085
  * Professor: Wladmir Cardoso Brandão
- * Complexiadade: O(N) -> Onde N é a quantidade de testes que serão realizadas durante os testes realizados para cada char
+ * Complexiadade: O(N) -> Onde N é a quantidade de caracteres testados que serão realizadas durante a validação realizados para cada char
 */
 
 #include <iostream>
@@ -19,10 +19,14 @@ bool buscaSubsequencia(string sequencia, string querie) {
     bool encontrar = false;
     int contador = 0;
 
-    for(int i = 0; i < sequencia.length() && encontrar == false; i++){
+    //Verifica char por char no intuito de descobrir a existência de subsequências
+    for(int i = 0; i < sequencia.length() && encontrar == false; i++) {
+        
         if(sequencia[i] == querie[contador]) {
             contador++;
         }
+        
+        //Caso a subsequencia pertença ao caso de teste
         if(contador == querie.length()) {
             encontrar = true;
         }
@@ -32,39 +36,39 @@ bool buscaSubsequencia(string sequencia, string querie) {
 }
 
 int main(int argc, char const *argv[]) {
-    int contador = 0;
+    int count_sequencias = 0;
+    int count_queries = 0;
+    int contador;
     int entrada;
-    int count_queries;
+    string sequencias;
 
     scanf("%d",&entrada);
 
-    string sequencias[entrada];
-    getline(std::cin, sequencias[contador]);
+    while (count_sequencias < entrada) {
+        contador = 0;
 
-    while(contador < entrada) {
-        getline(std::cin, sequencias[contador]);
-        contador++;     
-    }
+        cin.ignore(); // Desconta um \n após o uso de um scanf
+        getline(std::cin, sequencias);
 
-    contador = 0;
-    scanf("%d",&count_queries);
-    string queries[count_queries];
-    getline(std::cin, queries[contador]);
-
-
-    while(contador < count_queries) {
+        scanf("%d",&count_queries);
+        string queries[count_queries];
         getline(std::cin, queries[contador]);
-        contador++;  
-    }
 
-    for(int i = 0; i < entrada; i++) {
+        // Armazena todas as queries em um array de string
+        while(contador < count_queries) {
+            getline(std::cin, queries[contador]);
+            contador++; 
+        }
+
+        // Testa o caso de uso com todos os elementos do arrau de queries
         for (int j = 0; j < count_queries; j++) {
-            if (buscaSubsequencia(sequencias[i],queries[j])) {
+            if (buscaSubsequencia(sequencias, queries[j])) {
                 cout << "Yes" << endl;
             } else {
                 cout << "No" << endl;
             }
         }
-    }   
 
-}
+        count_sequencias++;
+    }
+}   
