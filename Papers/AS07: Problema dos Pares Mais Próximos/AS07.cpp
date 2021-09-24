42cf8eb5c74bf339f2281f90ff01a5028f3da963
Thiago Henriques <thiagohnogueira01@gmail.com>
2021-09-24 16:26:56 -0300

    [update] Activity: solution to AS07

--- Papers/AS07: Problema dos Pares Mais Próximos/AS07.cpp
+++ Papers/AS07: Problema dos Pares Mais Próximos/AS07.cpp
@@ -1,13 +1,12 @@
 /* 
- * Tarefa: Problema dos Pares Mais Próximos (Grafos)
+ * Tarefa: Problema dos Pares Mais Próximos (Geometria Computacional)
  * Disciplina: LABORATÓRIO DE PROJETO DE ALGORITMOS
  * Nome: Thiago Henriques Nogueira
  * Matricula: 655085
  * Professor: Wladmir Cardoso Brandão
- * Complexiadade: O(A * log A + A * log V) onde A = aresta e V = vertice
- *                Essa complexidade se deve pelo fato de ordenarmos o vetor de vertices em
- *                ordem crescente com base no peso das arestas e por causa do custo do algoritmo
- *                de Kruskal
+ * Complexiadade: O(N²) Para encontrar todas as distancias entre os pontos e verificar
+ *                qual delas será a menor distancia, é necessário passar por todo o vetor 
+ *                duas vezes. Logo aumentado a complexidade do código.
 */
 
 #include <iostream>
@@ -15,6 +14,14 @@
 #include <math.h>
 #include <bits/stdc++.h>
 
+// Definindo 10000
+#define INF 10000
+
+/*
+ * Calcula a distancia entre cada ponto
+ * @entrada vetor de pair de (int,int) que pega a coordenada de x e y de cada ponto
+ * @saida vetor de float armazenando todas as distancias entre os pontos
+*/
 std::vector<float> calculaDistancia(std::vector<std::pair<int,int>> pontos) {
     std::vector<float> distancias;
 
@@ -30,10 +37,15 @@ std::vector<float> calculaDistancia(std::vector<std::pair<int,int>> pontos) {
     return distancias;
 }
 
+/*
+ * Procura a menor distancia entre todas as distancias entre os pontos
+ * @entrada vetor de float contendo todas as distancias entre os pontos
+ * @saida float designado para armazenar a menor distancia entre dois pontos
+*/
 float menorDistancia(std::vector<float> distancias) {
-    float menor_distancia = distancias[0];
+    float menor_distancia = INF;
 
-    for(int i = 1; i < distancias.size(); i++) {
+    for(int i = 0; i < distancias.size(); i++) {
         
         if(menor_distancia > distancias[i]) {
             menor_distancia = distancias[i];
@@ -49,28 +61,35 @@ int main(int argc, char const *argv[]) {
     int cord_x;
     int cord_y;
 
-    scanf("%d",&count_pontos);
+    
+    std::cin >> count_pontos;
 
+    // Continua executando até o numero de pontos do conjunto ser igual a 0
     while (!count_pontos == 0) {
-        std::vector<std::pair<int,int>> pontos; 
         
-        for(int i = 0; i < count_pontos; i++) {
-            scanf("%d %d", &cord_x, &cord_y);
+        std::vector<std::pair<int,int>> pontos; 
+        std::vector<float> distancias;
+
+        // Armazena os pontos em um vetor
+        for(int i = 0; i < count_pontos; i++) { 
+            std::cin >> cord_x >> cord_y;
             pontos.push_back(std::make_pair(cord_x, cord_y));
         }
 
-        std::vector<float> distancias = calculaDistancia(pontos);
+        distancias = calculaDistancia(pontos);
         float menor_distancia = menorDistancia(distancias);
 
-        if(menor_distancia < 10000) {
-            std::cout << menor_distancia << std::endl;
+        // Verifica se a menor distancia é menor que 10000
+        if(menor_distancia < INF) {
+            std::cout << std::fixed << std::setprecision(4) << menor_distancia << std::endl;
         } else {
             std::cout << "INFINITY" << std::endl;
         }
 
+        // Limpa os vetores para um novo teste
         pontos.clear();
         distancias.clear();
 
-        scanf("%d",&count_pontos);
+        std::cin >> count_pontos;    
     }
 }   
